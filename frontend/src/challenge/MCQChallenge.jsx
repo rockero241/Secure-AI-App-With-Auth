10e891de52a883442489f78047cc31b4187d04dd
Luis O. <olivasl@berea.edu>
2025-07-01 21:06:12 -0400

    feat: little progress on MCQ challenge logic

--- frontend/src/challenge/MCQChallenge.jsx
+++ frontend/src/challenge/MCQChallenge.jsx
@@ -1,5 +1,21 @@
 import "react"
+import {useState} from "react"
 
-export function MCQChallenge() {
+export function MCQChallenge({challenge, showExplanation = false}) {
+    const [selectedOption, setSelectedOption] = useState(null)
+    const [shouldShowExplanation, setShouldShowExplanation] = useState(showExplanation)
+
+    // if sting convert to json, otherwise take existing options
+    const options = typeof challenge.options === "string"
+        ? JSON.parse(challenge.options)
+        : challenge.options
+
+    // if option is selected, grab index of answer and show explanation
+    const handleOptionSelect = (index) => {
+        if (selectedOption !== null) return;
+        setSelectedOption(index)
+        setShouldShowExplanation(true)
+    }
+
     return <></>
 }

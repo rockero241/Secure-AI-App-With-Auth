bd5d9fa2b8315b5faf99782a1ccad5dc27494e44
Luis O. <olivasl@berea.edu>
2025-07-06 12:30:45 -0400

    fix: addresses some DB syntax issues

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,25 +3,25 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-engine = create_engine("sqlite:///database.db", echo=True)
+engine = create_engine('sqlite:///database.db', echo=True)
 Base = declarative_base()
 
+
 class Challenge(Base):
-    # Defines the table name for the Challenge model in the database.
-    __tablename__ = "challenges"
+    __tablename__ = 'challenges'
 
     id = Column(Integer, primary_key=True)
-    dfficulty = Column(String, nullable=False)
+    difficulty = Column(String, nullable=False)
     date_created = Column(DateTime, default=datetime.now)
     created_by = Column(String, nullable=False)
+    title = Column(String, nullable=False)
     options = Column(String, nullable=False)
-    correct_answer_id = Column(String, nullable=False)
-    explanaition = Column(String, nullable=False)
+    correct_answer_id = Column(Integer, nullable=False)
+    explanation = Column(String, nullable=False)
 
 
 class ChallengeQuota(Base):
-    # This model tracks the number of challenges a user can attempt and when their quota was last reset.
-    __tablename__ = 'challenge_quota'
+    __tablename__ = 'challenge_quotas'
 
     id = Column(Integer, primary_key=True)
     user_id = Column(String, nullable=False, unique=True)
@@ -34,7 +34,6 @@ Base.metadata.create_all(engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
-    # Create a new database session using the SessionLocal configuration
     db = SessionLocal()
     try:
         yield db
